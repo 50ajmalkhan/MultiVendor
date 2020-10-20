936339ac5f57a7ff252c4e6e9a8ca619bd3c8aa9
MuhammadAjmalKhan <ajmalkhan50500@gmail.com>
2020-10-21 01:52:20 +0500

    adminLogin Completed

---
+++ src/actions/authaction.js
@@ -0,0 +1,31 @@
+import { authConstants } from './constant';
+import axios from '../helpers/axios'
+
+export const login = (user) => {
+
+    return async (dispatch) => {
+        dispatch({ type: authConstants.LOGIN_REQUEST })
+        const res = await axios.post('/admin/signin', {
+            ...user
+        });
+        if (res.status == 200) {
+            const { token, user } = res.data;
+            localStorage.setItem('token', token);
+            dispatch({
+                type: authConstants.LOGIN_SUCCESS,
+                payload: {
+                    token, user
+                }
+            });
+        }
+        else {
+            if (res.status == 400) {
+                dispatch({
+                    type: authConstants.LOGIN_FAILURE,
+                    payload: { error: res.data.error }
+                })
+            }
+        }
+
+    }
+}
--- src/containers/Signin/index.js
+++ src/containers/Signin/index.js
@@ -1,43 +1,68 @@
-import React  from 'react'; 
+import React, { useState } from 'react';
 import Layout from "../../components/Layout";
 import Input from "../../components/UI/Input"
-import {Container,Form,Row,Col,Button} from 'react-bootstrap';
-const Signin=(props)=>{
-    return(
-        
-           <Layout>
-               <Container>
-                 <Row style={{marginTop:"50px"}}>
-                     <Col md={{span:6,offset:3}}>
-                     <Form>
-                     <Input 
-                   label="Email" 
-                   placeholder="email"
-                   value=""
-                   type="email"
-                   onChange={()=>{}}
-                   />
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import { login } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom"
+const Signin = (props) => {
 
-            <Input 
-                   label="password" 
-                   placeholder="password"
-                   value=""
-                   type="password"
-                   onChange={()=>{}}
-                   />
-                 <Button variant="primary" type="submit">
-                     Submit
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { error, setError } = useState('')
+  const auth = useSelector(state => state.auth);
+
+  const dispatch = useDispatch();
+  const userLogin = (e) => {
+    e.preventDefault();
+    const user = {
+      email, password
+    }
+
+    dispatch(login(user));
+
+
+  }
+
+  if (auth.authenticate) {
+    return <Redirect to={'/'} />
+  }
+
+  return (
+    <Layout>
+      <Container>
+        <Row style={{ marginTop: "50px" }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form onSubmit={userLogin}>
+              <Input
+                label="Email"
+                placeholder="email"
+                value={email}
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <Input
+                label="password"
+                placeholder="password"
+                value={password}
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button variant="primary" type="submit">
+                Submit
                  </Button>
-                 
-         </Form>
-                     </Col>
-                 </Row>
-                 
-                        
-               </Container>
-           </Layout>
-       
-    )};
+
+            </Form>
+          </Col>
+        </Row>
 
 
-    export default Signin;
+      </Container>
+    </Layout>
+
+  )
+};
+
+
+export default Signin;
